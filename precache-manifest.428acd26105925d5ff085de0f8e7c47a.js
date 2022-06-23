38f83c1448e412e467008183ac5355e095382931
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-23 14:26:20 +0300

    Updates

---
+++ precache-manifest.428acd26105925d5ff085de0f8e7c47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddc062305cb464e57879133b3ded9d7",
+    "url": "/React-Color-Generator-Project/index.html"
+  },
+  {
+    "revision": "43334c971d7851aa1355",
+    "url": "/React-Color-Generator-Project/static/css/main.f6e8d1ab.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/React-Color-Generator-Project/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/React-Color-Generator-Project/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43334c971d7851aa1355",
+    "url": "/React-Color-Generator-Project/static/js/main.183c7e2d.chunk.js"
+  },
+  {
+    "revision": "42609886456b46fa690b",
+    "url": "/React-Color-Generator-Project/static/js/runtime-main.af5028fa.js"
+  }
+]);
